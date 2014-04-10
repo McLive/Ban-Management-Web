9279dbd225774b6790d7e14164506777b8f073a5
Mr_Maccrafter <Mr.Maccrafter@gmail.com>
2014-04-10 20:14:41 +0200

    German translation Part 1
    (validation.php unfinshed)

---
+++ ban-management/app/lang/de/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Passw&ouml;rter m&uuml;ssen mind. 6 Zeichen lang sein und m&uuml;ssen &uuml;bereinstimmen",
+
+	"user" => "Kein Benutzer mit dieser Email Addresse!",
+
+	"token" => "Password reset-token ist ung&uuml;ltig!",
+
+	"sent" => "Password-hilfe gesendet!",
+
+);
---
+++ ban-management/app/lang/de/site.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+
+	'ba' => 'Ban Management',
+	'language' => 'Sprache',
+	'players' => 'Spieler',
+	'player' => 'Spieler',
+	'stats' => 'Statistiken',
+	'appeal' => 'Ban Beschwerde',
+	'home' => 'Hauptseite',
+	'site' => 'Seite',
+	'admin' => 'Admin',
+	'dashboard' => 'Dashboard'
+
+);
