6b42eaabe0b836ed2f17f7978a348b852b3a8e33
Mr_Maccrafter <Mr.Maccrafter@gmail.com>
2014-04-16 15:36:28 +0200

    navbar improvements, navbar is now a separate file

---
+++ ban-management/app/views/site/nav.blade.php
@@ -0,0 +1,21 @@
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">{{ trans('site.ba') }}</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="#"><i class="fa fa-home"></i> {{ trans('site.home') }}</a></li>
+        <li><a href="#"><i class="fa fa-users"></i> {{ trans('site.players') }}</a></li>
+        <li><a href="#"><i class="fa fa-star"></i> {{ trans('site.stats') }}</a></li>
+        <li><a href="#"><i class="fa fa-comment"></i> {{ trans('site.appeal') }}</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</div>
