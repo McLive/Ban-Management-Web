833877ee5334ab9e91e2a463ebfa1d7325e32abf
Mr_Maccrafter <Mr.Maccrafter@gmail.com>
2014-04-21 19:30:55 +0200

    updated Template, created index backup

--- ban-management/app/views/site/template.blade.php
+++ ban-management/app/views/site/template.blade.php
@@ -5,27 +5,7 @@
     <title>{{ trans('site.ba') }}</title>
   </head>
   <body style="padding-top: 60px;">
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">{{ trans('site.ba') }}</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">{{ trans('site.home') }}</a></li>
-            <li><a href="#">{{ trans('site.players') }}</a></li>
-            <li><a href="#">{{ trans('site.stats') }}</a></li>
-            <li><a href="#">{{ trans('site.appeal') }}</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
+    @include('site/nav')
 
     <div class="container">
 
